7e0fadcee43c6ed0b16e6e139dbc1bcc12de9599
DESKTOP-6149RP4\palom <paloma.l.monteiro@gmail.com>
2021-10-14 17:21:51 -0300

    add documentation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
+import swagger from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerJson from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
-
+app.use("/docs", swagger.serve, swagger.setup(swaggerJson));
 app.use("/users", usersRoutes);
 
 export { app };
