f540b2eb2126fd22481b2678734638de4e58b7cf
Paloma Monteiro <paloma.monteiro@codding.com.br>
2021-11-22 18:03:17 -0300

    add localhost

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -3,20 +3,10 @@ import { Connection, createConnection, getConnectionOptions } from 'typeorm'
 export default async(): Promise<Connection> => {
     const defaultOptions = await getConnectionOptions();
 
-    try{
         return createConnection(
             Object.assign(defaultOptions, {
                 database: process.env.NODE_ENV === 'test'
                 ? "investimentos_test" : defaultOptions.database
             })
         )
-    }catch(error){
-        return createConnection(
-            Object.assign(defaultOptions, {
-                database: process.env.NODE_ENV === 'test'
-                ? "investimentos_test" : defaultOptions.database
-            })
-        )
-    }
-    
 }
